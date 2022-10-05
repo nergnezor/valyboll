4381be1952b827fae9c07f9c4d655479548f6b87
nergnezor <e@roseng.ren>
2022-10-05 10:08:59 +0200

    Initial commit

---
+++ main/game.go
@@ -0,0 +1,15 @@
+components {
+  id: "beach"
+  component: "/assets/beach.rivemodel"
+  position {
+    x: 251.0
+    y: 251.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
