b406f4918225c0e395dc7ec6033d4dae49201ed1
nergnezor <e@roseng.ren>
2022-10-05 13:57:40 +0200

    first state

--- main/game.go
+++ main/game.go
@@ -2,8 +2,23 @@ components {
   id: "beach"
   component: "/assets/beach.rivemodel"
   position {
-    x: 251.0
+    x: 249.0
     y: 251.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "main"
+  component: "/main/main.script"
+  position {
+    x: 0.0
+    y: 0.0
     z: 0.0
   }
   rotation {
